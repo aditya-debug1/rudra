19d358315d7bf1b06dab0fe54b3c7d1f37b7ff17
aditya <sr.aditya.dev@gmail.com>
2025-04-23 14:25:06 +0530

    updated breadcrumb hook added store code

--- client/src/hooks/use-breadcrumb.ts
+++ client/src/hooks/use-breadcrumb.ts
@@ -1,11 +1,24 @@
-import { useContext } from 'react';
-import { BreadcrumbContext } from '@/context/BreadcrumbContext';
+import { useContext } from "react";
+import { BreadcrumbContext } from "@/context/BreadcrumbContext";
+import { create } from "zustand";
 
 // Custom hook for accessing breadcrumb context
 export const useBreadcrumb = () => {
   const context = useContext(BreadcrumbContext);
   if (!context) {
-    throw new Error('useBreadcrumb must be used within a BreadcrumbProvider');
+    throw new Error("useBreadcrumb must be used within a BreadcrumbProvider");
   }
   return context;
 };
+
+interface BreadcrumbStore {
+  breadcrumbItems: { label: string; to?: string }[] | undefined;
+  setBreadcrumbItems: (
+    items: { label: string; to?: string }[] | undefined,
+  ) => void;
+}
+
+export const useBreadcrumbStore = create<BreadcrumbStore>((set) => ({
+  breadcrumbItems: undefined,
+  setBreadcrumbItems: (items) => set({ breadcrumbItems: items }),
+}));
